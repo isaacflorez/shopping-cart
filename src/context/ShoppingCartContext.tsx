818b716343f6b8b787684cf0cec22dd9ba3e9371
Isaac Florez <69365833+isaacflorez@users.noreply.github.com>
2022-07-09 16:34:34 -0700

    completed useLocalStorage hook so that shopping cart hold items even if page is refreshed. completed shopping cart total price and item details. No future updates to current app planned

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { useState, useContext, createContext, ReactNode } from "react";
 import {ShoppingCart} from '../components/ShoppingCart'
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type CartItem = {
     id: number
@@ -29,7 +30,7 @@ export function useShoppingCart(){
 
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps){
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", [])
     const [isOpen, setIsOpen] = useState(false)
     const openCart = () => setIsOpen(true)
     const closeCart = () => setIsOpen(false)
